e8fe3adff286010b27346755ed16ddfa27451ca9
dom <davidedomini@gmail.com>
2022-04-11 19:39:02 +0200

    added exps and libs

---
+++ src/main/java/exercise1/exp/InfiniteForceException.java
@@ -0,0 +1,5 @@
+package exercise1.exp;
+
+public class InfiniteForceException extends Exception {
+
+}
---
+++ src/main/java/exercise1/exp/NullVectorException.java
@@ -0,0 +1,5 @@
+package exercise1.exp;
+
+public class NullVectorException extends Exception {
+
+}
