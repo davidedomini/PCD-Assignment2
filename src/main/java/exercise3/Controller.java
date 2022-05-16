96ee20b59bc1134debbff7d68cc3ee0d4f4ca612
Veronika Folin <veronika.folin@studio.unibo.it>
2022-05-16 11:00:02 +0200

    fixed stop in ReactiveJavaParser

--- src/main/java/exercise3/Controller.java
+++ src/main/java/exercise3/Controller.java
@@ -3,20 +3,19 @@ package exercise3;
 import common.ControllerInterface;
 import common.Flag;
 import common.View;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public class Controller implements ControllerInterface {
 
-    private ReactiveJavaParser lib;
-    private Flag stopFlag;
+    private final ReactiveJavaParser lib;
     private View view;
     private Disposable d;
 
     public Controller(ReactiveJavaParser lib) {
         this.lib = lib;
-        this.stopFlag = new Flag();
     }
 
     public void setView(View view) {
@@ -25,21 +24,16 @@ public class Controller implements ControllerInterface {
 
     @Override
     public void startAnalysis(String srcDirectory) {
-        this.stopFlag.reset();
         System.out.println("Controller: start");
-        PublishSubject<String> streamReports = PublishSubject.create();
-        this.d = streamReports
+        Observable<String> ob = lib.analyzeProject(srcDirectory);
+        this.d = ob
                 .observeOn(Schedulers.computation())
-                .subscribe((v) -> {
-                    view.notifyUpdates(v);
-                });
-        lib.analyzeProject(srcDirectory, streamReports, stopFlag);
+                .subscribe(view::notifyUpdates);
     }
 
     @Override
     public void stopAnalysis() {
         System.out.println("Controller: stop");
-        this.stopFlag.stop();
         this.d.dispose();
     }
 }
